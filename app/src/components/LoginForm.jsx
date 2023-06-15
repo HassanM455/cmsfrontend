8cdefcea4adc73a82bec619e563012b00c89c770
Hassan Mahmood <hassanmahmood@Hassans-MacBook-Air.local>
2023-06-14 17:27:44 -0700

    Adding Focus event to Login Page by adding event handler

--- app/src/components/LoginForm.jsx
+++ app/src/components/LoginForm.jsx
@@ -1,21 +1,61 @@
+import {useState} from 'react';
 import './LoginForm.css';
 
 
 function LoginForm(props) {
 
-	const forgotPasswordHandler = (event) => {
+	const [inputUsername, setInputUsername] = useState('');
 
-	}
+	const [inputPassword, setInputPassword] = useState(''); 
+
+	const [isButtonFocus, setIsButtonFocus ] = useState(false) ;
+
+	const onInputUsernameChange = (event) => {
+		setInputUsername(event.target.value);
+		event.target.value ? setIsButtonFocus(true) : setIsButtonFocus(false);
+
+	};
+
+	const onInputPasswordChange = (event) => {
+		setInputPassword(event.target.value);
+		event.target.value ? setIsButtonFocus(true) : setIsButtonFocus(false);
+	};
+
+
 
 	return (
-		<div className="loginform"style={props.style}>
+		<div className="loginform" style={props.style}>
 			<header>Please Login</header>
-			<form>
-				<div className="loginform">
-					<input type="text" placeholder="Username *"/>
-					<input type="password" placeholder="Password *"/>
-					<button type="submit">Login</button>
-				</div>
+			<form className="loginform">
+					<div className="loginform__inputs_container">
+					<div className="loginform__input">
+						{(inputUsername || inputPassword ) && <p>Email *</p>}
+					</div>
+						<input 
+							className="loginform"
+							type="text" 
+							value={inputUsername}
+							onChange={onInputUsernameChange}
+							placeholder="Username *"
+						/>
+
+					<div className="loginform__input">
+						{(inputUsername || inputPassword ) && <p>Password *</p>}
+					</div>
+						<input
+							className="loginform"
+							type="password" 
+							placeholder="Password *"
+							onChange={onInputPasswordChange}
+							value={inputPassword}				
+						/>
+					</div>
+					<button 
+						className="loginform" 
+						type="submit"
+						style={{ opacity: isButtonFocus ? '1.0' : '0.5' }}>
+						Login
+					</button>
 			</form>
 		</div>
 	);
