7154b7d2a1b1aa45a7d2f4acbaf65ab6d72f531d
serenerain00 <104092388+serenerain00@users.noreply.github.com>
2023-06-15 23:52:58 -0400

    added responsive queries
    added responsive queries and added a pretty little image to the login

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm/LoginForm';
+import loginImage from './assets/login-img.jpg';
 
 function App() {
   return (
     <div className="app-login">
-    
+        <img src={loginImage} className="loginImage" alt="ocean"/>
         <LoginForm>
         </LoginForm>
     </div>
